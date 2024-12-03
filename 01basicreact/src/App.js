0b7bbf617afbd1bbfb209d8037814f830f44ce69
raghav vashistha <raghavvashistha9@gmail.com>
2024-12-04 00:49:58 +0530

    commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <hi>React first project | hey this is raghav vashistha and i'm learning react  from free resources and official documentation of reacjs,</hi>
+  );
+}
+
+export default App;
